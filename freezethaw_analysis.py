2fe3e93d512b2a6ccae7ce7d3d7249fbc3166536
Samuel Morton <samuel.h.morton.21@dartmouth.edu>
2020-02-06 18:57:36 -0500

    finished testing calc_thaw_freeze

--- freezethaw_analysis.py
+++ freezethaw_analysis.py
@@ -19,12 +19,21 @@ January 31, 2020
 import pandas as pd
 
 
-TMAX_LOC = 3
-TMAX_FLAG_LOC = 4
-TMIN_LOC = 5
-TMIN_FLAG_LOC = 6
+MONTH_LOC = 2
+DAY_LOC = 3
+YEAR_LOC = 4
+TMAX_LOC = 5
+TMAX_FLAG_LOC = 6
+TMIN_LOC = 7
+TMIN_FLAG_LOC = 8
+TAVG_LOC = 9
 UNACCEPTABLE_FLAGS = ['I']  # data flags that indicate unreliable data
 DATA = 'data/paamiut_tempdata2.xlsx'
+JANUARY = 1
+AUGUST = 8
+MONTH = 30
+FREEZING_TEMP = 0
+THAW_FREEZE_INTERVAL = 40
 
 
 def open_df(filename):
@@ -68,13 +77,122 @@ def data_check(df, idx):
     return True
 
 
+def locate_year_beginning(df, year, start_idx):
+    '''
+    Locate the df index of the start of a given year. Return None if the start_idx
+    is beyond the first month of the given year
+    df - the DataFrame
+    year - the year you want the spring estimate for
+    start_idx - the given starting index
+    '''
+    
+    # check that the start_idx doesn't put you beyond the year
+    if df.iloc[start_idx, YEAR_LOC] > year or (df.iloc[start_idx, YEAR_LOC] == year and df.iloc[start_idx, MONTH_LOC] > JANUARY):
+        return None
+    
+    # get a list of the indicies in the df
+    indicies = list(df.index)
+    
+    # loop through the df rows until you find January of the given year
+    for i in range(start_idx, len(indicies)):
+        if df.iloc[i, YEAR_LOC] == year and df.iloc[i, MONTH_LOC] == JANUARY:
+            return i
+    
+    return None
 
+
+def estimate_spring_date(df, temp_metric, year, start_idx = 0):
+    '''
+    Estimate the date of "Spring," or the date in which temperatures stay above
+    freezing for the rest of the growing season. The method assumes that freezing
+    temperatures won't happen once August begins, and that if there hasn't been
+    freezing temperatures for a month, then Spring began on that first non-freezing
+    day. Returns the df index of the day in question.
+    df - the DataFrame
+    temp_metric - the way in which temperature will be assessed; the data has
+    temp max, temp min, and temp avg.
+    year - the year you want the spring estimate for
+    start_index - specify a starting index for the method to cut down on iteration
+    time
+    '''
+    
+    # locate the beginning index of the given year; return None if the starting index is beyond the specified year
+    year_idx = locate_year_beginning(df, year, start_idx)
+    
+    # if year_idx is None, return None
+    if year_idx is None:
+        return None
+    
+    # init track for spring beginning
+    spring = year_idx 
+    
+    while True:
         
+        # if it's been a month since the last below freezing day end the loop
+        if year_idx - spring >= MONTH:
+            return year_idx - MONTH
+        
+        if df.iloc[year_idx, temp_metric] < FREEZING_TEMP:
+            spring = year_idx + 1
+        
+        year_idx += 1
     
+    return year_idx
+
+
+def thaw_freeze(df, idx, temp_metric):
+    '''
+    Check if there is a "thaw-freeze" event at the given index. A "thaw-freeze"
+    event is when a day in which the average temp is above freezing is followed
+    by a day in which the average temp is below freezing.
+    df - the DataFrame
+    idx - idx of the day in the data to check
+    temp_metric - the way in which temperature will be assessed; the data has
+    temp max, temp min, and temp avg.
+    '''
     
+    # if the given day has a temp above freezing
+    if df.iloc[idx, temp_metric] > FREEZING_TEMP:
+        
+        # if the following day is then below freezing
+        if df.iloc[idx + 1, temp_metric] <= FREEZING_TEMP:
+            return True
+        
+        # if the following day is then above freezing
+        else:
+            return False
     
+    # if the given day is below freezing then there is no thaw-freeze event
+    else:
+        return False
+
+
+def calc_thaw_freeze(df, year_idx):
+    '''
+    Calculate the number of thaw-freeze events in the time leading up to a given
+    date
+    '''
+    
+    # init counter of thaw-freeze events
+    count = 0
+    print("first i is {} on {} {}".format(year_idx - THAW_FREEZE_INTERVAL, df.iloc[year_idx - THAW_FREEZE_INTERVAL, MONTH_LOC], df.iloc[year_idx - THAW_FREEZE_INTERVAL, MONTH_LOC]))
+    for i in range(year_idx - THAW_FREEZE_INTERVAL, year_idx - 1):
+        if thaw_freeze(df, i, TAVG_LOC):
+            count += 1
+    
+    return count
+    
+
 # === Main Function ===
 df = open_df(DATA)
+year_idx = estimate_spring_date(df, TAVG_LOC, 1959)
+print("year is {} on {} {}".format(year_idx, df.iloc[year_idx, MONTH_LOC], df.iloc[year_idx, DAY_LOC]))
+
+count = calc_thaw_freeze(df, year_idx)
+print("Count is {}".format(count))
+
+
+
 
 
 

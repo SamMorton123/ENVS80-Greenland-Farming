b3ed3aecb2f0903680bbda06563a4418a188f5b2
Samuel Morton <samuel.h.morton.21@dartmouth.edu>
2020-02-19 18:51:21 -0500

    end of project

--- paamiut_images.py
+++ paamiut_images.py
@@ -15,14 +15,22 @@ import freezethaw_analysis as fta
 
 
 XLABEL = "Year"
-YLABEL = "Number of early spring thaw-freeze events"
-TITLE = "Change In the Number of Annual Thaw-Freeze Events Over Time"
+YLABEL = "Frequency of Thaw-Freeze Events"
+TITLE = "Frequency of Thaw-Freeze Events in Early Spring"
 XBEGIN = 1955
 XEND = 2020
-XSTEP = 5
+XSTEP = 10
 YBEGIN = 0
 YEND = 10
 YSTEP = 1
+TIMES = "Times New Roman"
+FONT = 12
+FONT2 = 14
+FIGNAME = "thawfreeze_paamiut.png"
+TREND = "y = 0.002x + 0.378"
+LAB_X = 0.2
+LAB_Y1 = 0.8
+LAB_Y2 = 0.75
 
 
 def timeseries_image(filename, plot_type = "scatter"):
@@ -37,12 +45,27 @@ def timeseries_image(filename, plot_type = "scatter"):
     
     if plot_type == "scatter":
         plt.plot(years, tfs, 'o', color = 'black')
-    plt.xlabel(XLABEL)
-    plt.ylabel(YLABEL)
-    plt.xticks(np.arange(XBEGIN, XEND + 1, step = XSTEP))
-    plt.yticks(np.arange(YBEGIN, YEND + 1, step = YSTEP))
-    plt.title(TITLE)
     
+    # graph aesthetics
+    plt.xlabel(XLABEL, fontname = TIMES, fontsize = FONT)
+    plt.ylabel(YLABEL, fontname = TIMES, fontsize = FONT)
+    plt.xticks(np.arange(XBEGIN, XEND + 1, step = XSTEP), fontname = TIMES, fontsize = FONT)
+    plt.yticks(np.arange(YBEGIN, YEND + 1, step = YSTEP), fontname = TIMES, fontsize = FONT)
+    plt.title(TITLE, fontname = TIMES, fontsize = FONT2)
+    
+    # trendline
+    z = np.polyfit(years, tfs, 1)
+    p = np.poly1d(z)
+    plt.plot(years, p(years),"b--")
+    
+    eq = TREND
+    R = np.corrcoef(years, tfs)
+    R2 = "R^2 = {}".format(round(R[0][1] ** 2, 4))
+    
+    plt.figtext(LAB_X, LAB_Y1, eq)
+    plt.figtext(LAB_X, LAB_Y2, R2)
+    
+    plt.savefig(FIGNAME)
     
 
 
